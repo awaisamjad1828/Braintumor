5fae6be480aada04c7aef0f4152edb0fc771b48a
Awais Amjad <2022cs541@student.uet.edu.pk>
2024-11-22 23:37:05 +0500

    update

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from myapp.views import UserProgressView
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/user-progress/<str:user_id>/', UserProgressView.as_view(), name='total-enrolled-courses'),
+   
 ]
